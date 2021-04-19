7ea8bd302c2d12146fd0edaeb3e0637a9b746ac8
meanother <juicehq@yandex.ru>
2021-04-20 00:46:15 +0300

    get started the udemy course

---
+++ part2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+// run go run main.go
+
+// go run - compile and go
+// go build  - compiles
+// go fmt  - format text files
+// go install - install packeges 
+// go get - got download a package
+// go test - run and execute all  test
+
+
+//  golang.org/pkg
+// https://github.com/StephenGrider/GoCasts/tree/master/diagrams
---
+++ part3/cards/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	// this 2 lines arre 100% equivalent!
+
+	card = "Five of Diamonds"
+	fmt.Println(card)
+}
