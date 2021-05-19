7f75079e5fb13f493f57b47e758332d8b6731ef7
meanother <juicehq@yandex.ru>
2021-05-20 01:14:45 +0300

    day 14

--- part7/mainx.go
+++ part7/mainx.go
@@ -13,16 +13,29 @@ func main() {
 		"https://newquiz.fl-bankrotstvo.ru/",
 		"https://golang.org",
 	}
+
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link + " might be down!")
+		c <- link
 		return
 	}
 	fmt.Println(link + " is up!")
+	// c <- "Yes, its up!"
+	c <- link
 }
