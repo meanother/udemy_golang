5740dcb1ea8345ca6c6908a29828f0f068504efd
meanother <juicehq@yandex.ru>
2021-05-21 01:28:32 +0300

    day 15

--- part7/mainx.go
+++ part7/mainx.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -23,12 +24,20 @@ func main() {
 	// for i := 0; i < len(links); i++ {
 	// 	fmt.Println(<-c)
 	// }
-	for {
-		go checkLink(<-c, c)
+	// for {
+	// 	go checkLink(<-c, c)
+	// }
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 }
 
 func checkLink(link string, c chan string) {
+	// time.Sleep(time.Second)
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link + " might be down!")
@@ -39,3 +48,11 @@ func checkLink(link string, c chan string) {
 	// c <- "Yes, its up!"
 	c <- link
 }
+
+
+1-2
+2-3
+3-4
+4-2
+5-1
+6-1
